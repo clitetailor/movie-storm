98175aa082d8fe702bae7b839bf3bc991c951f24
clitetailor <ducnhatphung@gmail.com>
2017-11-25 13:17:11 +0700

    search complete

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { MovieService } from '../movie.service';
+import { SearchService } from '../search.service';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/catch';
+
 
 @Component({
   selector: 'app-home-page',
@@ -19,24 +25,72 @@ export class HomePageComponent implements OnInit {
     };
   });
 
+  @ViewChild('searchInput') searchInput;
+
+  private inputDebounceSubscription: Subject<string> = new Subject();
+  private searchSubscription: Subject<string> = new Subject();
+  private suggestions = [];
+  private searchString = '';
+
   constructor(
     private router: Router,
     private ngZone: NgZone,
-    private authService: AuthService,
-    private movieService: MovieService
+    private movieService: MovieService,
+    private searchService: SearchService
   ) { }
 
-  ngOnInit() {
+  public ngOnInit() {
+    this.getMovies();
+
+    this.bindSuggestions();
+    this.bindSearch();
+  }
+
+  private search(rawString) {
+    if (rawString && rawString !== '') {
+      this.searchString = '';
+      this.searchInput.nativeElement.blur();
+      this.searchSubscription.next(rawString);
+    }
+  }
+
+  private getMovies() {
     this.movieService.getMovies()
       .then((movies: any[]) => {
-        this.ngZone.run(() => {
-          this.movies.forEach((movie, i) => {
-            Object.assign(movie, movies[i]);
-          });
+        this.movies.forEach((movie, i) => {
+          Object.assign(movie, movies[i]);
         });
       })
       .catch(err => {
         console.error(err);
+      });
+  }
+
+  private onSearchInput(value) {
+    this.inputDebounceSubscription.next(value);
+  }
+
+  private bindSuggestions() {
+    this.inputDebounceSubscription
+      .debounceTime(400)
+      .mergeMap(raw => this.searchService.suggest(raw))
+      .subscribe((suggestions: any[]) => {
+        this.suggestions = suggestions;
+      }, err => {
+        return new Subject();
+      });
+  }
+
+  private bindSearch() {
+    this.searchSubscription
+      .mergeMap(raw => this.searchService.search(raw))
+      .subscribe((suggestions: any[]) => {
+
+        if (suggestions.length > 3) {
+          this.movies = suggestions.slice(0, 9);
+        }
+      }, err => {
+        return new Subject();
       });
   }
 
@@ -59,6 +113,8 @@ export class HomePageComponent implements OnInit {
   }
 
   public navigateToMovie(i) {
+    console.log(i);
+
     if (i > 0) {
       this.router.navigate(['movie', i]);
     }
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -8,11 +8,15 @@ export class SearchService {
     private http: HttpService
   ) { }
 
-  public suggest(raw: String) {
-    this.http.get(`suggest/${this.http.base64(raw)}`);
+  public suggest(raw: String | undefined) {
+    if (raw === undefined || raw === '') {
+      return this.http.get(`suggest`);
+    }
+
+    return this.http.get(`suggest/${this.http.base64(raw)}`);
   }
 
-  public search(raw: String) {
-    this.http.get(`search/${this.http.base64(raw)}`);
+  public search(raw: String | undefined) {
+    return this.http.get(`search/${this.http.base64(raw)}`);
   }
 }
